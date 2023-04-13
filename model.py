c0f76bb02524ff4cc5acc9a7af6933b8017b0ffb
Tamara Vashchuk <ms.vashchuk96@gmail.com>
2023-04-13 15:36:45 -0700

    Add classmethod get reservations

--- model.py
+++ model.py
@@ -1,9 +1,8 @@
 """Models for reservation scheduler app."""
-import os
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import UniqueConstraint
 
 db = SQLAlchemy()
+
 
 class User(db.Model):
     __tablename__ = "users"
@@ -12,62 +11,58 @@ class User(db.Model):
     username = db.Column(db.String(50), nullable=False, unique=True)
 
     def __repr__(self):
-          return f"<User id={self.id} username={self.username}>"
-    
+        return f"<User id={self.id} username={self.username}>"
+
     @classmethod
-    def create_user(cls, username):
-         username = username.lower()
-         user = cls(username=username)
+    def create_user(cls, username: str):
+        username = username.lower()
+        # noinspection PyArgumentList
+        user = cls(username=username)
 
-         db.session.add(user)
-         db.session.commit()
-
-         return user
-    
+        db.session.add(user)
+        db.session.commit()
+        return user
 
     @classmethod
     def get_user_by_username(cls, username):
-         
-         return cls.query.filter(cls.username == username).first()
-    
+        return cls.query.filter(cls.username == username).first()
 
     @classmethod
     def get_user(cls):
-         
-         return cls.query.all()
-         
+        return cls.query.all()
 
-     
+
 class Reservation(db.Model):
-     __tablename__ = "reservations"
+    __tablename__ = "reservations"
+    __table_args__ = (db.UniqueConstraint('reservation_date', 'time_slot', name='uc_1'),
+                      db.UniqueConstraint('user_id', 'reservation_date', name='uc_2'))
 
-     reservation_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-     reservation_date = db.Column(db.DateTime(timezone=True))
-     time_slot = db.Column(db.String(20), nullable=False)
+    reservation_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    reservation_date = db.Column(db.DateTime(timezone=True))
+    time_slot = db.Column(db.String(20), nullable=False)
 
-     user = db.relationship("User", backref="reservations")
+    user = db.relationship("User", backref="reservations")
 
-     def __repr__(self):
-          return f"<Reservation reservation_id={self.reservation_id} username={self.username} start_time={self.start_time}>"
-     
+    def __repr__(self):
+        return f"<Reservation reservation_id={self.reservation_id} user_id={self.user_id} start_time={self.reservation_date}>"
 
-     @classmethod
-     def new_reservation(cls, user_id, datetime_object, timeslot):
+    @classmethod
+    def new_reservation(cls, user_id, datetime_object, timeslot):
         """ Create and add a new reservation """
-
+        # noinspection PyArgumentList
         reservation = cls(user_id=user_id, reservation_date=datetime_object, time_slot=timeslot)
-
         db.session.add(reservation)
         db.session.commit()
         return reservation
 
-    
-
+    @classmethod
+    def get_reseravations(cls, user_id):
+        """get all reservations for user"""
+        return cls.query.filter(cls.user_id == user_id).all()
 
 
 def connect_to_db(flask_app, db_uri="postgresql:///reservation", echo=True):
-
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     flask_app.config["SQLALCHEMY_ECHO"] = echo
     flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
